898a6641fcff7722c37605c3e3118b79ff18d8a3
s-hosoai <shintaro.hosoai@gmail.com>
2026-04-07 01:10:12 +0000

    Deploying to gh-pages from @ hosoai-lab/hosoai-lab.github.io@b2e7b780463971749fc0a2578aaf8c5268ce731e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-members",
+          title: "members",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/members/";
+          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
